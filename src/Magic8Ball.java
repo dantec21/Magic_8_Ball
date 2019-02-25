3c08d51484de3a14bb687bf4bdb65dc552d75205
Dante <dante.collier.2022@student.lhschools.org>
2019-02-25 13:38:51 -0500

    Introduction code

---
+++ src/Magic8Ball.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Magic8Ball {
+
+    public static void main(String[]args){
+        //declare
+        Scanner keyboard;
+
+        int testing;
+        String question;
+
+
+        //initialize
+
+        System.out.println("I am a magic 8 ball, think of a question then type a number 1-100 and I will answer.");
+        
+
+
+
+    }
+}
