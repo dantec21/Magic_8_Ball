92f0b426cce595a9f56b4cc61a70537f9a76dd77
Dante <dante.collier.2022@student.lhschools.org>
2019-02-25 14:00:17 -0500

    Else if conditianals written

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -4,16 +4,37 @@ public class Magic8Ball {
 
     public static void main(String[]args){
         //declare
-        Scanner keyboard;
+        Scanner keyboard = new Scanner(System.in);
 
         int testing;
-        String question;
+        int question;
 
 
         //initialize
 
         System.out.println("I am a magic 8 ball, think of a question then type a number 1-100 and I will answer.");
-        
+        question = keyboard.nextInt();
+        if (question==1-10)
+            ;
+        else if (question==11-20)
+            ;
+        else if (question==21-30)
+            ;
+        else if (question==31-40)
+            ;
+        else if (question==41-50)
+            ;
+        else if (question==51-60)
+            ;
+        else if (question==61-70)
+            ;
+        else if (question==71-80)
+            ;
+        else if (question==81-90)
+            ;
+        else if (question==91-100)
+            ;
+        else ;
 
 
 
